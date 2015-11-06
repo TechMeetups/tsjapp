04a6be93c26f4e1d54ed85abbfed45cfc787de9a
jayeshdalwadi <jayeshdalwadi2007@gmail.com>
2015-11-06 23:55:41 +0530

    change in lable

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,12 @@
 App.info({
     name: 'CC Integration',
-    description: 'Graphically manage your Projects & Processes',
+    description: 'Constant Contact manage your Email Markating',
     version: '0.0.1'
 });
 App.icons({
-    'android_ldpi': 'public/assets/img/constant-contact-share-logo.gif',
-    'android_mdpi': 'public/assets/img/constant-contact-share-logo.gif',
-    'android_hdpi': 'public/assets/img/constant-contact-share-logo.gif',
-    'android_xhdpi': 'public/assets/img/constant-contact-share-logo.gif'
+    'android_ldpi': 'public/assets/img/cc_logo.png',
+    'android_mdpi': 'public/assets/img/cc_logo.png',
+    'android_hdpi': 'public/assets/img/cc_logo.png',
+    'android_xhdpi': 'public/assets/img/cc_logo.png'
 });
 App.accessRule("*");
