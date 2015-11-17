b59a63c241a64411fbf2ede9f010e81876e6c6c3
jayeshdalwadi <jayeshdalwadi2007@gmail.com>
2015-11-17 22:09:18 +0530

    campain count

--- client/template/dashboard/tabs/tabsThree.js
+++ client/template/dashboard/tabs/tabsThree.js
@@ -25,43 +25,53 @@ Template.tabsThree.helpers({
       var clicked_report = cc_clicked.find({user_id:Meteor.userId(),email_address:data.email}).fetch()
       var open_report = cc_opened_report.find({user_id:Meteor.userId(),email_address:data.email}).fetch()
       var send_report = cc_send_report.find({user_id:Meteor.userId(),email_address:data.email}).fetch()
+      console.log(clicked_report);
+      console.log(open_report);
+      console.log(send_report);
       var uniqueCampain =[]
-      for(var i=0;i<clicked_report.lenght;i++){
+      for(var i=0;i < clicked_report.length;i++){
+        console.log(clicked_report[i].campaign_id);
         uniqueCampain.push(clicked_report[i].campaign_id)
       }
-      for(var i=0;i<open_report.lenght;i++){
+      for(var i=0;i<open_report.length;i++){
         uniqueCampain.push(open_report[i].campaign_id)
       }
-      for(var i=0;i<send_report.lenght;i++){
+      for(var i=0;i<send_report.length;i++){
         uniqueCampain.push(send_report[i].campaign_id)
       }
+      console.log(uniqueCampain);
       uniqueCampain = $.unique(uniqueCampain);
-      for(var i= 0;i < uniqueCampain.length;i++){
+      console.log(uniqueCampain);
+      for(var j= 0;j < uniqueCampain.length;j++){
         var clicked_count = 0;
         var open_count = 0;
         var send_count = 0;
 
-        for(var i=0;i<clicked_report.lenght;i++){
-          if(clicked_report[i].campaign_id == uniqueCampain[i] ){
+        for(var i=0;i<clicked_report.length;i++){
+          if(clicked_report[i].campaign_id == uniqueCampain[j] ){
             clicked_count+=1;
           }
         }
-        for(var i=0;i<open_report.lenght;i++){
-          if(open_report[i].campaign_id == uniqueCampain[i] ){
+        for(var i=0;i<open_report.length;i++){
+          if(open_report[i].campaign_id == uniqueCampain[j] ){
             open_count+=1;
           }
         }
-        for(var i=0;i<send_report.lenght;i++){
-          if(send_report[i].campaign_id == uniqueCampain[i]){
+        for(var i=0;i<send_report.length;i++){
+          if(send_report[i].campaign_id == uniqueCampain[j]){
             send_count+=1;
           }
         }
-        finaldata.push({campain:cc_campaign.find({user_id:Meteor.userId(),id:uniqueCampain[i]}),clicked_count:clicked_count,open_count:open_count,send_count:send_count});
+        console.log(uniqueCampain[j]);
+        var campain = cc_campaign.findOne({user_id:Meteor.userId(),id:uniqueCampain[j]});
+        campain["clicked_count"] = clicked_count;
+        campain["open_count"] = open_count;
+        campain["send_count"] = send_count;
+        finaldata.push(campain);
+        console.log(finaldata)
       }
-    }else{
-
     }
-    return cc_campaign.find({user_id:Meteor.userId()})
+    return finaldata;
   }
 });
 
