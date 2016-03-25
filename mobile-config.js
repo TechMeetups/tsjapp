6b20a61b84ab99b8c9f158851777ce8f1ee071f3
techmeetups <shawn@techmeetups.com>
2016-03-25 08:32:26 +0200

    new logo

--- mobile-config.js
+++ mobile-config.js
@@ -34,9 +34,9 @@ App.launchScreens({
 });
 
 App.icons({
-    'android_ldpi': 'public/assets/img/tsjair_logo.png',
-    'android_mdpi': 'public/assets/img/tsjair_logo.png',
-    'android_hdpi': 'public/assets/img/tsjair_logo.png',
-    'android_xhdpi': 'public/assets/img/tsjair_logo.png'
+    'android_ldpi': 'public/assets/img/tsjfair_logo.png',
+    'android_mdpi': 'public/assets/img/tsjfair_logo.png',
+    'android_hdpi': 'public/assets/img/tsjfair_logo.png',
+    'android_xhdpi': 'public/assets/img/tsjfair_logo.png'
 });
 App.accessRule("*");
