337285e3e12236ae9ca41484a2966ad7464d1f23
techmeetups <shawn@techmeetups.com>
2016-03-28 20:09:18 +0300

    jobs for company

--- client/template/events/tabs/companyDetails.js
+++ client/template/events/tabs/companyDetails.js
@@ -1,25 +1,37 @@
 JOB_INCREMENT=10
-Template.companyDetails.created = function () {
+Template.companyDetails.created = function () 
+{
   Session.set("job_limit",JOB_INCREMENT)
-  this.autorun(function () {
+  this.autorun(function () 
+  {
     this.subscription = Meteor.subscribe('company_details',Router.current().params._id,Router.current().params._company_id);
-    this.subscription_job = job_manager.default_subscribe(Router.current().params._company_id);
+    this.subscription_job = job_manager.default_subscribe(null,Router.current().params._company_id);
   }.bind(this));
 };
-Template.companyDetails.rendered = function () {
+
+Template.companyDetails.rendered = function () 
+{
   this.autorun(function () {
-    if (!this.subscription.ready() || !this.subscription_job.ready()) {
+    if (!this.subscription.ready() || !this.subscription_job.ready()) 
+    {
       IonLoading.show();
-    } else {
+    } 
+    else 
+    {
       IonLoading.hide();
     }
   }.bind(this));
 };
-Template.companyDetails.helpers({
-  format_date : function(date){
+
+Template.companyDetails.helpers(
+{
+  format_date : function(date)
+  {
     return company_manager.format_data(date)
   },
-  jobs_list : function(){
+  
+  jobs_list : function()
+  {
    return job_manager.getList();
   },
   moreTasks  : function()
@@ -31,6 +43,7 @@ Template.companyDetails.helpers({
     return "/tabs/job/"+Router.current().params._company_id+"/"+_id;
   },
 });
+
 Template.companyDetails.events(
 {
   'click #showMoreResults' : function(event, template)
